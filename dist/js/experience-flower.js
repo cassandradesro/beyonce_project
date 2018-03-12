29f636392a1b02b25b8308417d6399a2831d6161
cassandradesro <cdesro10@gmail.com>
2018-03-11 22:04:51 -0400

    commit flower-power

--- dist/js/experience-flower.js
+++ dist/js/experience-flower.js
@@ -1,4 +1,18 @@
-"use strict";
+'use strict';
 
 console.log("transition.js");
+
+var flowerMusic = new Howl({
+  src: ['dist/flower_sequence/flower_sound.mp3'],
+  autoplay: true,
+  loop: true,
+  volume: 1,
+  onend: function onend() {
+    console.log('flowerMusic Finished!');
+  }
+});
+
+flowerMusic.play();
+
+// TweenMax.staggerTo(".flower", 1, { scale: 2.0, repeat: -1}, 0.1);
 //# sourceMappingURL=experience-flower.js.map
--- src/js/experience-flower.js
+++ src/js/experience-flower.js
@@ -1,1 +1,16 @@
 console.log("transition.js");
+
+var flowerMusic = new Howl({
+  src: ['dist/flower_sequence/flower_sound.mp3'],
+  autoplay: true,
+  loop: true,
+  volume: 1,
+  onend: function onend() {
+    console.log('flowerMusic Finished!');
+  }
+});
+
+flowerMusic.play();
+
+// TweenMax.staggerTo(".flower", 1, { scale: 2.0, repeat: -1}, 0.1);
+

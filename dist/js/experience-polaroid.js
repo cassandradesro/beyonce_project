aea8cc4b0776366fd3f012a358ea1e11e58f8285
cassandradesro <cdesro10@gmail.com>
2018-06-03 14:46:23 -0400

    commit

--- dist/js/experience-polaroid.js
+++ dist/js/experience-polaroid.js
@@ -31,4 +31,34 @@ document.body.addEventListener("keyup", function (e) {
 
 	$audio.play();
 });
+
+document.body.addEventListener("touchstart", function (e) {
+	if (e.keyCode == 13) {
+		console.log("you hit enter!");
+		var nextHidden = document.querySelector(".hidden");
+		if (nextHidden) {
+			nextHidden.play();
+			nextHidden.classList.remove("hidden");
+			currentMusicIndex = (currentMusicIndex + 1) % 14; //modulo
+			var updateAudio = "dist/polaroid_video/Audio" + currentMusicIndex + ".mp3";
+			$audio.setAttribute('src', updateAudio); // animate it into place
+		}
+		nextHidden = document.querySelector(".hidden");
+		if (nextHidden) {
+			// preloading!
+			nextHidden.load();
+			var preloadAudio = new Audio();
+			preloadAudio.src = "dist/polaroid_video/Audio" + (currentMusicIndex + 1) + ".mp3";
+		}
+	}
+	if (e.keyCode == 32) {
+		$audio.pause();
+		$audio.currentTime = 0;
+	}
+});
+
+document.body.addEventListener("touchend", function (e) {
+
+	$audio.play();
+});
 //# sourceMappingURL=experience-polaroid.js.map
